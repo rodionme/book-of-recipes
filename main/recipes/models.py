b877e6b1002b4795ae390d2bf471513f92568ad9
Rodion Mescheryakov <msrodion@gmail.com>
2018-03-10 23:55:02 +0200

    Add Recipe and Ingredient models. Add some testing models

--- main/recipes/models.py
+++ main/recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    # type
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
+    # image(s?)
+    # cousine
+
+    def __str__(self):
+        return self.name
