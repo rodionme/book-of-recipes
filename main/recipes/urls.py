3bf52dffa390d5b21a8e5295eb1f3e3b6531e760
Rodion Mescheryakov <msrodion@gmail.com>
2018-03-11 18:16:18 +0200

    Add example views and templates

--- main/recipes/urls.py
+++ main/recipes/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, {'app': {'name': 'Book of Recipes'}}, name='index'),
+    path('', views.index, name='index'),
+    path('recipes/', views.recipes, name='recipes'),
+    path('recipes/<int:recipe_id>', views.recipe, name='recipe'),
+    path('ingredients/', views.ingredients, name='ingredients'),
+    path('ingredients/<int:ingredient_id>', views.ingredient, name='ingredient'),
 ]
--- main/recipes/views.py
+++ main/recipes/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Recipe, Ingredient
 
 
-def index(request, app):
-    return HttpResponse('Hello ' + app['name'] + '!')
+def index(request):
+    return render(request, 'recipes/index.html')
+
+
+def recipes(request):
+    recipes = Recipe.objects.all()
+
+    return render(request, 'recipes/recipes.html', {'recipes': recipes})
+
+
+def recipe(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+
+    return render(request, 'recipes/recipe.html', {'recipe': recipe})
+
+
+def ingredients(request):
+    ingredients = Ingredient.objects.all()
+
+    return render(request, 'recipes/ingredients.html', {'ingredients': ingredients})
+
+
+def ingredient(request, ingredient_id):
+    ingredient = Ingredient.objects.get(id=ingredient_id)
+
+    return render(request, 'recipes/ingredient.html', {'ingredient': ingredient})
